0ddf9f3ce7ed2ebc64a341a3f08d431acef773a7
AD41963 <Shaik.kousar@lumen.com>
2024-01-10 14:20:17 +0530

    content changed

--- src/main/java/com/example/demo/apis/SampleApi.java
+++ src/main/java/com/example/demo/apis/SampleApi.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class SampleApi {
     @GetMapping
     public ResponseEntity<String > get(){
-        return ResponseEntity.ok("Hello, master");
+        return ResponseEntity.ok("Hello, master1");
     }
 
 }
